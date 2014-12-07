5f5f7797d3af8baedb66f92670284269d85bfa66
gabrielgatu <gabriel.dny@gmail.com>
2014-12-07 15:26:39 +0100

    Exercise1 - Generics and Custom List

---
+++ prova.scala
@@ -0,0 +1,23 @@
+object App extends App {
+  implicit def valueUtils[T](value: T) = new Utils(value)
+
+  class Utils[T](value: T) {
+    implicit def print = println(value) }
+
+  trait Comprehension[T] {
+    def filter(predicate: T => Boolean)
+  }
+
+  class RichList[T](values: Seq[T]) extends Comprehension[T] {
+    private val list = values.foldLeft(List[T]()) { (list, elem) => elem :: list }.reverse
+
+    override def toString = list.toString()
+    override def filter(predicate: T => Boolean): List[T] = for (e <- list if predicate(e)) yield e
+  }
+  object RichList {
+    def apply[T](values: T*) = new RichList[T](values)
+  }
+
+  val list = RichList(1, 2, 3, 4, 5)
+  list.filter(_ % 2 == 0).print
+}
